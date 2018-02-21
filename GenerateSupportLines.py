32a2c9dc41e7b45b3dbac740f0991b1ab7a9f80d
Omar  Elbarmawi <oelbarmawi@berkeley.edu>
2018-02-20 20:45:16 -0800

    Finished TODOs, optimized functions

--- GenerateSupportLines.py
+++ GenerateSupportLines.py
@@ -5,73 +5,79 @@ import matplotlib.dates as mdates
 import numpy as np
 from scipy.signal import argrelextrema
 from datetime import datetime
+from sys import maxsize
 
-# data = pd.read_csv("Goog180.csv", low_memory=False)
-# print(data)
+""" 
+dates 	- numpy array of datetime objects used for plotting
+prices 	- numpy array of closing stock price for corresponding date in 'dates'
+"""
+min_price, max_price = maxsize, -maxsize 
+dates, prices = np.array([]), np.array([])
+
 with open('Siri180.csv') as file:
 	reader = csv.reader(file)
-	dates = []
-	prices = []
 	for row in reader:
-		if row[1] != 'Close' and row[0] != 'Date': # TODO: get rid of this check
-			#Converting string date to datetime object in order to plot
-			dates.append(datetime.strptime(row[0] , '%m/%d/%y %H:%M'))
-			prices.append(float(row[1]))
+		if row[0] == 'Date' or row[1] == 'Price':
+			continue
+		p = float(row[1])
+		dates = np.append(dates, datetime.strptime(row[0] , '%m/%d/%y %H:%M'))
+		prices = np.append(prices, p)
+		min_price = min(p, min_price)
+		max_price = max(p, max_price)
 
-# Build plot
+
+"""Build the plot with x-axis as dates and y-axis as prices"""
 plt.plot(dates, prices, color='green')
-# Draw horizontal line
-# plt.axhline(y=5.75, xmin=0, xmax=200, linewidth=2, color = 'red')
-# plt.axhline(y=5.44, xmin=0, xmax=200, linewidth=2, color = 'red')
 
 def between(lower, upper, val):
 	return lower <= val and val <= upper
 
-# Convert lists to numpy arrays for future functions
-date_array = np.array(dates)
-price_array = np.array(prices)
 
-# TODO: Find min and max values within first loop
-min_val = min(prices) 
-max_val = max(prices)
+print("max: $", min_price)
+print("min: $", max_price)
 
-print(min_val)
-print(max_val)
+"""For local maxima"""
+maxima = argrelextrema(prices, np.greater)
 
-# for local maxima
-print("local maxima")
-maxima = argrelextrema(price_array, np.greater)
+"""For local minima"""
+minima = argrelextrema(prices, np.less)
 
-lo = min_val
-hi = min_val
-delta = abs(max_val - min_val)* 0.05
+critical_points = np.append(maxima[0], minima[0])
+
+delta = abs(max_price - min_price) * 0.05
 price_points = {}
 
-# Create bins
-while (hi < max_val):
-	hi = lo + delta # 5% delta price
-	mid = round((lo + hi)/2, 2)
-	price_points[mid] = 0
-	for m in maxima[0]:
-		if between(lo, hi, prices[m]):
-			price_points[mid] += 1;
-	lo = hi
+""" This while loop creates bins of price ranges and
+tests to see which bin each individual price falls under """
+for c in critical_points:
+	lo = min_price
+	hi = min_price
+	while (hi < max_price):
+		hi = lo + delta
+		mid = round((lo + hi) / 2, 2)
+		if between(lo, hi, prices[c]):
+			if mid in price_points:
+				price_points[mid] += 1
+			else:
+				price_points[mid] = 1
+			break
+		lo = hi
 
-
-# Sort the keys according to the values:
+"""
+sorted_prices: an array of sorted keys for the dictionary 'price_points'
+price_points (dictionary)
+key:	price bin
+value: 	number of local maxima within in this bin (TODO: update this to include minima)
+"""
 sorted_prices = sorted(price_points, key=price_points.__getitem__, reverse=True)
-for k in sorted_prices:
-    print("{} : {}".format(k, price_points[k]))
-    if price_points[k] >= 5:
-    	plt.axhline(y=k, xmin=0, xmax=200, linewidth=2, color = 'red')
 
-# for k in sorted_prices:
-# # print([prices[m] for m in maxima])
+"""Draws top 3 bins with most data points within them"""
+for k in range(5):
+	val = sorted_prices[k]
+	print("{} : {}".format(val, price_points[val]))
+	# Create a horizontal support line
+	plt.axhline(y=val, xmin=0, xmax=200, linewidth=2, color = 'red')
 
-# # for local minima
-# print("local minima")
-# minima = argrelextrema(date_array, np.less)
-# # print([dates[mi] for mi in minima])
 
-# # Display plot 
+""" Display Plot """
 plt.show()
