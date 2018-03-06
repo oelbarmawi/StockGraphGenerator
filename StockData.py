7922eee63e53e647b93f53eb15b463360167bc6b
Omar  Elbarmawi <oelbarmawi@berkeley.edu>
2018-03-06 02:48:32 -0800

    Minor adjustments

--- StockData.py
+++ StockData.py
@@ -5,7 +5,6 @@ from sys import maxsize, exit
 from time import sleep
 from multiprocessing import Pool as ThreadPool
 import certifi
-# import os
 import smtplib
 
 """Cycle repeats every 'sleep_time' seconds"""
@@ -20,8 +19,9 @@ value: target prices (tuple of floats)
 targets = {}
 username, password = "", ""
 fromaddr, toaddr = "", ""
-output_file = open('output.txt', 'a')
 current_time = ""
+output_file = open('output.txt', 'a')
+
 
 def sendEmail(subject, message_body):
 	global username, password, fromaddr, toaddr
@@ -34,49 +34,6 @@ def sendEmail(subject, message_body):
 		server.quit()
 	except:
 		print("Unable to send email.")
-
-def getLiveData(ticker):
-	global targets, output_file, current_time
-	
-	info = []
-	# info.append(border)
-
-	stop_loss = targets[ticker][0]
-	target_price = targets[ticker][1]
-
-	"""Scrape data from secure http server"""
-	url = "http://finance.yahoo.com/quote/" + ticker + "?/?p=" + ticker
-	http = PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
-	r = http.request('GET', url)
-
-	if (r.status != 200):
-		print("Something Went Wrong\nStatus Code:", r.status)
-		return
-		
-	soup = BeautifulSoup(r.data, 'lxml')
-	p = soup.find("div", { "class": "My(6px) smartphone_Mt(15px)"})
-	current_price = p.find("span", { "class": "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"}).text.strip()
-	current_price = float(current_price.replace(",", ""))
-
-	info.append("\n{} price:\t\t${}".format(ticker, current_price))
-
-	"""Notify me if a stop-loss or a target price has been hit!"""
-	
-	
-	if current_price < stop_loss:
-		info.append(" ---> STOP LOSS hit:\t${}".format(stop_loss))
-		message = "The current price of {} is ${} -- Time: {}".format(ticker, current_price, current_time)
-		subj = "Stop-Loss hit! Sell {}!".format(ticker)
-		# sendEmail(subj, message)
-
-	elif current_price >= target_price:
-		info.append(" ---> TARGET hit:\t${}".format(target_price))
-		message = "The current price of {} is ${} -- Time: {}".format(ticker, current_price, current_time)
-		subj = "Target hit! Sell {}".format(ticker)
-		# sendEmail(subj, message)
-
-	return ''.join(info)
-	
 
 def getEmailCredentials(filename):
 	global username, password, fromaddr, toaddr
@@ -106,16 +63,59 @@ def readStockData(filename):
 	f.close()
 	return targets.keys()
 
+def getLiveData(ticker):
+	global targets, output_file, current_time
+	
+	info = []
+	stop_loss = targets[ticker][0]
+	target_price = targets[ticker][1]
+
+	"""Scrape data from secure http server"""
+	url = "http://finance.yahoo.com/quote/" + ticker + "?/?p=" + ticker
+	http = PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+	r = http.request('GET', url)
+
+	if (r.status != 200):
+		print("Something Went Wrong\nStatus Code:", r.status)
+		return
+	
+	"""Parsing through returned html code to find stock price"""
+	soup = BeautifulSoup(r.data, 'lxml')
+	p = soup.find("div", { "class": "My(6px) smartphone_Mt(15px)"})
+	current_price = p.find("span", { "class": "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"}).text.strip()
+	current_price = float(current_price.replace(",", ""))
+
+	info.append("\n{} price:\t\t${}".format(ticker, current_price))
+
+	"""Notify me if a stop-loss or a target price has been hit!"""
+	if current_price < stop_loss:
+		info.append(" ---> STOP LOSS hit:\t${}".format(stop_loss))
+		message = "The current price of {} is ${} -- Time: {}".format(ticker, current_price, current_time)
+		subj = "Stop-Loss hit! Sell {}!".format(ticker)
+		# sendEmail(subj, message)
+
+	elif current_price >= target_price:
+		info.append(" ---> TARGET hit:\t${}".format(target_price))
+		message = "The current price of {} is ${} -- Time: {}".format(ticker, current_price, current_time)
+		subj = "Target hit! Sell {}".format(ticker)
+		# sendEmail(subj, message)
+
+	return ''.join(info)
+
 def main():
 	global output_file, current_time
+
 	ticker_keys = readStockData('dummy_data.txt')
 	getEmailCredentials('sensitive.txt')
 	border = "\n" + ("*" * 30)
+	num_threads = 10
 
 	"""Begin web scraping"""
 	while True:
 		try:
-			pool = ThreadPool(10)
+			"""Creating 'num_threads' to run the getLiveData() function"""
+			pool = ThreadPool(num_threads)
+			"""results is a list of return values from getLiveData"""
 			results = pool.map(getLiveData, ticker_keys)
 			current_time = "TIME: {}".format(datetime.now().time())
 			print(current_time)
@@ -130,7 +130,6 @@ def main():
 			sleep(sleep_time)
 		except KeyboardInterrupt:
 			print("\nExiting...")
-			output_file.write("\n")
 			output_file.close()
 			exit() #System call
 		
